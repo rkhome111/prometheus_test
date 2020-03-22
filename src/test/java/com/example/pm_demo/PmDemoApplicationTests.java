2431c5ace402f21b525c0f805449039359688dbc
roushan kumar Singh <roushansingh.singh.ext@gmail.com>
2020-03-22 14:13:16 +0530

    prometheus project added

---
+++ src/test/java/com/example/pm_demo/PmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
